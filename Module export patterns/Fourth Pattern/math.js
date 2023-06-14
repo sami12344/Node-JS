20e6eeeba3fe71a203c2512f632d829f0d67ebb7
sami12344 <freelancersami16203@gmail.com>
2023-06-15 01:51:49 +0600

    Module Exports Pattern

---
+++ Module export patterns/Fourth Pattern/math.js
@@ -0,0 +1,8 @@
+export.add = (a, b) => {
+  return a + b
+}
+
+exports.substract = (a, b) => {
+  return a - b
+}
+
---
+++ Module export patterns/Third pattern/index.js
@@ -0,0 +1,9 @@
+const math = require('./math')
+const {add , substract} = math
+
+
+console.log(add(2, 3))
+console.log(substract(2, 3))
+
+console.log(math.add(2,3));
+console.log(math.substract(2, 3))
