105c88f1f1c8352b43546abe6d8c67d020652912
sami12344 <freelancersami16203@gmail.com>
2023-06-15 00:43:29 +0600

    Module Wrapper Function

---
+++ module wrapper/iffe.js
@@ -0,0 +1,13 @@
+(function(){
+ const superHero = 'Superman'
+ console.log(superHero);
+
+})()
+
+(function(){
+const superHero = 'Batman'
+console.log(superHero);
+})()
+
+
+// this is how iffe works. node js uses modules like this way
---
+++ module wrapper/index.js
@@ -0,0 +1,9 @@
+require('./batman')
+require('./superman')
+
+// output =>>
+
+//  batman 
+// superman
+
+/*No conflicts because node js use every module iffe or wraper function */
